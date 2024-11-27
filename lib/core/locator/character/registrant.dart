8eb8e683c520105237ba1082c5b2b55a4c3eb94f
Ibrahim Farajzade <ibrahimfarajzade@gmail.com>
2024-11-27 15:55:09 +0400

    Add locator

---
+++ lib/core/locator/character/registrant.dart
@@ -0,0 +1,36 @@
+import 'package:graphql_flutter/graphql_flutter.dart';
+import 'package:rick_and_morty_characters/core/locator/locator_registrant.dart';
+import 'package:rick_and_morty_characters/data/datasources/character_remote_data_source.dart';
+import 'package:rick_and_morty_characters/data/repositories/character_repository_impl.dart';
+import 'package:rick_and_morty_characters/domain/repositories/character_repository.dart';
+import 'package:rick_and_morty_characters/domain/usecases/get_characters.dart';
+import 'package:rick_and_morty_characters/presentation/bloc/character_bloc.dart';
+import 'package:rick_and_morty_characters/qraphql/client.dart';
+
+class CharacterLocatorRegistrant implements LocatorRegistrant {
+  @override
+  Future<void> register() async {
+    // Register GraphQL Client as a singleton
+    locator.registerLazySingleton(() => GraphQLConfig.initializeClient());
+
+    // Register Remote Data Source
+    locator.registerLazySingleton<CharacterRemoteDataSource>(
+      () => CharacterRemoteDataSourceImpl(client: locator.get<GraphQLClient>()),
+    );
+
+    // Register Repository
+    locator.registerLazySingleton<CharacterRepository>(
+      () => CharacterRepositoryImpl(remoteDataSource: locator.get<CharacterRemoteDataSource>()),
+    );
+
+    // Register Use Case
+    locator.registerLazySingleton(
+      () => GetCharacters(repository: locator.get<CharacterRepository>()),
+    );
+
+    // Register Bloc
+    locator.registerFactory(
+      () => CharacterBloc(getCharacters: locator.get<GetCharacters>()),
+    );
+  }
+}
